455ab77e68ca0d8ba50c7d2dd2b6366dde0b5415
BriarSokil <ruslanmodiagin@gmail.com>
2023-11-01 19:48:54 +0200

    Convert sql date output function

--- Client/js/displayReadings.js
+++ Client/js/displayReadings.js
@@ -12,10 +12,13 @@ function showCart()
             $.each(result.readings, function(){
                 console.log(this.id);
                 $("#history-table-body").append(
-                    "<tr><td>"+this.id+"</td><td>"+this.city+"</td><td>"+this.date+"</td><td>"+this.temperature+"</td><td>"+this.description+"</td><td><img src='"+this.icon+"' width='100px'></td><td>"+this.windspeed+"km/h</td><td>"+this.humidity+"%</td></tr>"
+                    "<tr><td>"+this.id+"</td><td>"+this.city+"</td><td>"+convertSQLDateOutput(this.date)+"</td><td>"+this.temperature+" &#8451;</td><td>"+this.description+"</td><td><img src='"+this.icon+"' width='50px'></td><td>"+this.windspeed+"km/h</td><td>"+this.humidity+"%</td></tr>"
                 );
             });
             $("#cart-table-body").append("<tr></tr>");
         }
     });
+}
+function convertSQLDateOutput(date){
+    return date.split("-").reverse().join("/");
 }
